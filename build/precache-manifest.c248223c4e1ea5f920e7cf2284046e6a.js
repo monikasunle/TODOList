f18f37a77d621e833667ab733407037c166913ea
monikasunle <2680655949@qq.com>
2020-10-03 10:02:22 +0800

    todolist

---
+++ build/precache-manifest.c248223c4e1ea5f920e7cf2284046e6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a2eb01d0fbe04aaea90c67b3159c8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b32bd928cdda712f6696",
+    "url": "/static/js/2.1413a434.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1413a434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "255b0ebc455254cb2c2d",
+    "url": "/static/js/main.4f3eb05c.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
